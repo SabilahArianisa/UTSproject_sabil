a38d091632f1978a72a6fa8df685bd886e037a6e
Akhmad Ridlo <akhmadd432@gmail.com>
2024-10-27 18:07:53 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
@@ -0,0 +1,48 @@
+package lat.pam.utsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ListFoodActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FoodAdapter
+    private lateinit var foodList: List<Food>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_food)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Menyiapkan data makanan
+        foodList = listOf(
+            Food("Batagor", "Batagor asli enak dari Bandung", R.drawable.batagor),
+            Food("Black Salad", "Salad segar yang dibuat secara langsung", R.drawable.black_salad),
+            Food("Cappucino", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.cappuchino)
+        )
+
+        adapter = FoodAdapter(foodList, this::onFoodItemClick) // Pass the click listener function
+        recyclerView.adapter = adapter
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun onFoodItemClick(food: Food) {
+        val intent = Intent(this, OrderActivity::class.java)
+        intent.putExtra("foodName", food.name)
+        startActivity(intent)
+    }
+}
